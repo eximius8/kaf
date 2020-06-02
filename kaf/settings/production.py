e7694a7d01dd9f00d05e6c3166206e1a38dcd849
mikhail.trunov <mikhail.trunov@gmail.com>
2020-06-02 23:45:18 +0400

    Secret exposed

--- kaf/settings/production.py
+++ kaf/settings/production.py
@@ -20,5 +20,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-env = os.environ.copy()
-SECRET_KEY = env['SECRET_KEY']
+#env = os.environ.copy()
+SECRET_KEY = "a#yv!z8j(lmrffxxl^+9t!mr)izbrf)7n2cx9(+c$tp1!)j*e5"
